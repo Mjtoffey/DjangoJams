9e07c0f6a8771f5b41ee3aa3d30edf12d64cac24
Matthew Toffey <134286170+Mjtoffey@users.noreply.github.com>
2023-08-03 16:28:53 +0000

    changed installed apps

--- mysite/jams/models.py
+++ mysite/jams/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Song(models.Model):
+    id = models.AutoField
+    name = models.CharField(max_length=200)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+class Artist(models.Model):
+    id = models.AutoField
+    name = models.CharField(max_length=200)
+class Album(models.Model):
+    id = models.AutoField
+    name = models.CharField(max_length=200)
+class Genre(models.Model):
+    id = models.AutoField
+    name = models.CharField(max_length=200)
