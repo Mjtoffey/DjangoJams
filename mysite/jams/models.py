554f5accc3c0ad8a2ee57863cdc338506ae92568
Matthew Toffey <134286170+Mjtoffey@users.noreply.github.com>
2023-08-04 12:44:09 +0000

    made migrations

--- mysite/jams/models.py
+++ mysite/jams/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class Songs(models.Model):
     name = models.CharField(max_length=200)
-    genre = models.ManyToManyField('Genres')
-    artist = models.ManyToManyField('Artists')
-    album = models.ForeignKey(on_delete=models.CASCADE)
+    genre = models.ManyToManyField('Genres', related_name='songs')
+    artist = models.ForeignKey('Artists', on_delete=models.SET_NULL, null=True)
+    album = models.ForeignKey('Albums', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -20,6 +20,8 @@ class Artists(models.Model):
 
 class Albums(models.Model):
     title = models.CharField(max_length=200)
+    artist = models.ForeignKey('Artists', on_delete=models.CASCADE)
+    genre = models.ForeignKey('Genres', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
--- mysite/jams/views.py
+++ mysite/jams/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import *
+from .serializers import *
 
-# Create your views here.
+
+class GenreListView(generics.ListCreateAPIView):
+    queryset = Genres.objects.all()
+    serializer_class = GenreSerializer
+
+
+class ArtistListView(generics.ListCreateAPIView):
+    queryset = Artists.objects.all()
+    serializer_class = ArtistSerializer
+
+
+class AlbumListView(generics.ListCreateAPIView):
+    queryset = Albums.objects.all()
+    serializer_class = AlbumSerializer
+
+
+class SongListView(generics.ListCreateAPIView):
+    queryset = Songs.objects.all()
+    serializer_class = SongSerializer
